f4a5cfbb4e66804257a06ad8292eef8f2b479f75
kuwisdelu <k.bemis@northeastern.edu>
2019-05-24 16:55:19 -0400

    use Cardinal:::.selectRegion()

--- R/module-selectView.R
+++ R/module-selectView.R
@@ -4,14 +4,6 @@ selectView <- function(input, output, session, dataset) {
   clicks <- reactiveValues(x = c(), y = c())
   
   
-  .selectRegion <- function(loc, pdata, subset, axs = c("x", "y")) {
-    roi <- rep(FALSE, nrow(pdata))
-    coord <- coord(pdata)[subset,axs,drop=FALSE]
-    selected <- sp::point.in.polygon(coord[,1], coord[,2], loc$x, loc$y) > 0
-    roi[subset] <- selected
-    roi
-  }
-  
   data <- reactive({
 		tryCatch(get(dataset, envir=globalenv()),
 			error=function(e) NULL)
@@ -32,7 +24,8 @@ selectView <- function(input, output, session, dataset) {
   
   observeEvent(input$button_select, {
 
-      roi <- reactive({ .selectRegion(clicks, pixelData(data()),
+      roi <- reactive({
+        Cardinal:::.selectRegion(clicks, pixelData(data()),
                 subset = plot_image()$subset, 
                 axs = plot_image()$coordnames) })
       
